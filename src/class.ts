70bc447daee5988931e5253dde2bc067175afb7a
Akash Perera <akash.perera@moht.com.sg>
2024-07-25 10:38:12 +0800

    add part 1

--- src/class.ts
+++ src/class.ts
@@ -1,43 +1,51 @@
-import {timer} from "./decorators/timer";
-import {log} from "./decorators/log";
+import { timer } from "./decorators/timer";
+import { log } from "./decorators/log";
+import { cache } from "./decorators/cache";
 
 export class MyClass {
-    @log
-    primeMethodRecurcion(n: number) {
-        const isPrime = (num: number) => {
-            for (let i = 2, sqrtNum = Math.sqrt(num); i <= sqrtNum; i++) {
-                if (num % i === 0) return false;
-            }
-            return num > 1;
-        }
+  // @log
+  @timer()
+  @cache()
+  primeMethodRecurcion(n: number) {
+    const isPrime = (num: number) => {
+      for (let i = 2, sqrtNum = Math.sqrt(num); i <= sqrtNum; i++) {
+        if (num % i === 0) return false;
+      }
+      return num > 1;
+    };
 
-        let primeCount = 0;
-        for (let i = 2; i <= n; i++) {
-            if (isPrime(i)) primeCount++;
-        }
-        return primeCount;
+    let primeCount = 0;
+    for (let i = 2; i <= n; i++) {
+      if (isPrime(i)) primeCount++;
     }
+    return primeCount;
+  }
 
-    @log
-    primeMethodLoop(num: number) {
-        const res = [];
-        for (let i = 2; i <= num; i++) {
-            let flag = 0;
-            for (let j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    flag = 1;
-                    break;
-                }
-            }
-
-            if (i > 1 && flag == 0) {
-                res.push(i);
-            }
+  // @log
+  @timer()
+  @cache()
+  primeMethodLoop(num: number) {
+    const res = [];
+    for (let i = 2; i <= num; i++) {
+      let flag = 0;
+      for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+          flag = 1;
+          break;
         }
-        return res.length;
+      }
+
+      if (i > 1 && flag == 0) {
+        res.push(i);
+      }
     }
+    return res.length;
+  }
 }
 
-const myCl = new MyClass()
+const myCl = new MyClass();
 
-console.log(myCl.primeMethodRecurcion(10000))
+console.log(myCl.primeMethodRecurcion(100000));
+console.log(myCl.primeMethodRecurcion(200000));
+console.log(myCl.primeMethodRecurcion(100000));
+// console.log(myCl.primeMethodLoop(200000));
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,45 @@
 const log = console.log;
 
 let apt = {
-    floor: 12,
-    number: '12B',
-    size: 3400,
-    bedRooms: 3.4,
-    bathRooms: 2,
-    Price: 400000,
-    amenities: {
-        airCon: 4,
-    }
+  floor: 12,
+  number: "12B",
+  size: 3400,
+  bedRooms: 3.4,
+  bathRooms: 2,
+  Price: 400000,
+  amenities: {
+    airCon: 4,
+  },
 };
 
+// override the floor property to be read-only
+Object.defineProperty(apt, "floor", {
+  writable: false,
+});
 
-const descriptor = Object.getOwnPropertyDescriptor(apt, 'floor')
+// override the floor property to be read-only
+Object.defineProperty(apt, "floor", {
+  enumerable: false,
+});
 
+// override the floor property to be non-configurable
+Object.defineProperty(apt, "floor", {
+  configurable: false,
+});
 
-log(descriptor)
-log(apt.floor)
+const descriptor = Object.getOwnPropertyDescriptor(apt, "floor");
+
+// before overridden: { value: 12, writable: true, enumerable: true, configurable: true }
+// after overridden: { value: 12, writable: false, enumerable: true, configurable: true }
+log(descriptor);
+
+// before overridden: [ 'number', 'size', 'bedRooms', 'bathRooms', 'Price', 'amenities', 'floor' ]
+// after overridden: [ 'number', 'size', 'bedRooms', 'bathRooms', 'Price', 'amenities' ]
+log(Object.keys(apt));
+
+// will throw an error, because we have disabled configurability
+// Object.defineProperty(apt, "floor", {
+//   writable: true,
+// });
+
+log(apt.floor);
